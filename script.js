bbffdc538123970dc80b8ee826e00c5c6cd92845
RD <raghavdv@protonmail.ch>
2024-06-09 10:31:26 +0530

    Minor edits

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  // Set the date we're counting down to
-  const countDownDate = new Date("August 17, 2024 08:00:00").getTime();
+  const countDownDate = new Date("August 29, 2024 08:00:00").getTime();
 
-  // Update the count down every 1 second
   const countdownFunction = setInterval(() => {
-    // Get today's date and time
     const now = new Date().getTime();
     
-    // Find the distance between now and the count down date
     const distance = countDownDate - now;
     
-    // Time calculations for days, hours, minutes and seconds
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
     const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
-    // Output the result in an element with id="countdown"
     document.getElementById("days").innerText = days;
     document.getElementById("hours").innerText = hours;
     document.getElementById("minutes").innerText = minutes;
     document.getElementById("seconds").innerText = seconds;
     
-    // If the count down is finished, write some text
     if (distance < 0) {
       clearInterval(countdownFunction);
-      document.getElementById("countdown").innerHTML = "EXPIRED";
+      document.getElementById("countdown").innerHTML = "";
+      document.getElementById("countdown-title").innerHTML = "";
     }
   }, 1000);
 });
